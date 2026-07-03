95226d07752de39300653728a5781c3bc7532be3
Kyren82 <erykkal123@gmail.com>
2026-07-03 11:47:14 +0200

    docker-compose.yaml: changed port, important folders added & main site and sub sites are still under works

--- index.php
+++ index.php
@@ -4,11 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Piekar</title>
+    <link rel="stylesheet" href="styles/theme.css">
     <link rel="stylesheet" href="styles/index-style.css">
+    <link rel="icon" href="assets/cynamon-icon.png">
 </head>
 <body>
     <div id="wrapper">
-        
+        <ul class="nav">
+            <li><a href="index.php">Strona główna</a></li>
+            <li><a href="subsites/shop.php">Sklep</a></li>
+            <li><a href="subsites/about.php">O nas</a></li>
+            <li><a href="#">Cocis</a></li>
+        </ul>
+        <div class="hero">
+            <div class="hero-panel">
+                <h1 class="hero-title">Wielki fikuśny napis</h1>
+                <p>Bułki prosto z pieca *PEAK*</p>
+            </div>
+        </div>
+        <div class="main">
+
+        </div>
+        <div class="footer">
+
+        </div>
     </div>
 </body>
 </html>
---
+++ subsites/shop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piekar</title>
+    <link rel="stylesheet" href="../styles/theme.css">
+    <link rel="stylesheet" href="../styles/shop-style.css">
+    <link rel="icon" href="assets/cynamon-icon.png">
+</head>
+<body>
+    <div id="wrapper">
+        <?php
+            require_once("../headers/nav.html");
+        ?>
+        <div class="main">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+</html>
